65cc67d30b767b1c7e9252b163c62acae467cf40
Nigel Fernandes <dev@nigel.in>
2010-10-04 23:07:38 +1100

    initial drop :Nigel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubycraft::Application.config.secret_token = '93bd88c54d24bdfe7891bc3e9773b2f1bef354a5810b2d729c9e9dcb9f5c93731d201c037c9b2e1dfd6df583ded31c72940809f26f9c05e1dcee3a20faf5b2b4'
